ee74490f814f18ab5b6a965e58cb11bc8f5804f7
dinaranjo <diegonaranjo303@gmail.com>
2025-04-29 07:29:46 -0500

    dearrollo front end

--- FrontEnd/trujilloyasociados/src/App.jsx
+++ FrontEnd/trujilloyasociados/src/App.jsx
@@ -3,7 +3,14 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Trujillo y asociados</h1>
+      <div id='app' className=' relative h-screen p-2 gap-2'>
+        <aside className='bg-stone-500 [grid-area:aside] flex-col flex overflow-y-auto'>
+            aside
+        </aside>
+        <main className='[grid-area:main] bg-blue-300'> main
+        </main>
+        <footer className='bg-red-400 [grid-area:footer] ' >footer</footer>
+      </div>
     </>
   )
 }
