39bd006b544abff5aad664f526e0de0dc3df0c7c
dinaranjo <diegonaranjo303@gmail.com>
2025-05-20 01:17:04 -0500

    deje de usar git flow y estoy subiendo todo a main pero es por tiempo, pero bueno

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import './index.css'
 
+
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
 <AuthProvider>
